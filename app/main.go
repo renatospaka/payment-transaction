e06b2d564d9a276318e3f4b315a2d78831c284bc
Renato Spakauskas <renatospaka@gmail.com>
2023-06-13 19:58:02 -0300

    feat: :sparkles: postgres repository
    initializing a postgres repository to handle data persistence

--- app/main.go
+++ app/main.go
@@ -1,37 +1,22 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"log"
 
 	_ "github.com/lib/pq"
-	"github.com/renatospaka/transaction/utils/configs"
+	repository "github.com/renatospaka/transaction/adapter/postgres"
+	// "github.com/renatospaka/transaction/utils/configs"
 )
 
 func main() {
 	log.Println("iniciando a aplicação")
-	configs, err := configs.LoadConfig(".")
-	if err != nil {
-		log.Panic(err)
-	}
+	// configs, err := configs.LoadConfig(".")
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
 
 	//open connection to the database
-	log.Println("iniciando conexão com o banco de dados")
-	conn := "postgresql://" + configs.DBUser + ":" + configs.DBPassword + "@" + configs.DBHost + "/" + configs.DBName + "?sslmode=disable"
-	db, err := sql.Open("postgres", conn)
-	if err != nil {
-		log.Panic(err)
-	}
-	defer db.Close()
+	repository.NewPostgresDatabase()
 
-	log.Println("banco de dados conectado")
-	ctx := context.Background()
-	pb, err := db.Conn(ctx)
-	if err != nil {
-		log.Panic(err)
-	}
-	defer pb.Close()
-
-
+	
 }
