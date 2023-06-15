3d6fb339115dce11ffa6dc77226ad8e141350d12
Renato Spakauskas <renatospaka@gmail.com>
2023-06-15 15:42:58 -0300

    fix: :bug: usecase find
    find one transaction
    
    is is breaking dua to PrepareContext along with QueryRow are not working

--- adapter/postgres/findTransaction.go
+++ adapter/postgres/findTransaction.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 	"log"
 
 	"github.com/renatospaka/payment-transaction/core/dto"
@@ -10,37 +11,51 @@ import (
 
 func (p *PostgresDatabase) findTransaction(ctx context.Context, id string) (*entity.Transaction, error) {
 	log.Println("repository.transactions.findTransaction")
+	//
 	query := `
-	SELECT id, status, value, approved_at, denied_at, created_at, updated_at, deleted_at
+	SELECT id, status, value , approved_at, denied_at, created_at, updated_at, deleted_at
 	FROM transactions
 	WHERE id = $1`
 	stmt, err := p.DB.PrepareContext(ctx, query)
 	if err != nil {
+		log.Printf("repository.transactions.findTransaction - err: %v\n", err)
 		return nil, err
 	}
 	defer stmt.Close()
 
-	rows, err := stmt.QueryContext(ctx, id)
+	rows := stmt.QueryRow(ctx, id)
 	if err != nil {
+		log.Printf("repository.transactions.findTransaction - err: %v\n", err)
 		return nil, err
 	}
-	defer rows.Close()
 
 	var tr *dto.TransactionDto
-	log.Println("vai executar rows.Next()")
-	for rows.Next() {
-		log.Println("vai executar rows.Scan")
-		err = rows.Scan(&tr.ID, &tr.Status, &tr.Value, &tr.ApprovedAt, &tr.DeniedAt, &tr.CreatedAt, &tr.UpdatedAt, &tr.DeletedAt)
-		if err != nil {
-			log.Printf("err :%v\n", err)
-			return nil, err
+	log.Println("vai executar rows.Scan()")
+	err = rows.Scan(&tr.Status, &tr.Value , &tr.ApprovedAt, &tr.DeniedAt, &tr.CreatedAt, &tr.UpdatedAt, &tr.DeletedAt)
+	// , &tr.ApprovedAt, &tr.DeniedAt, &tr.CreatedAt, &tr.UpdatedAt, &tr.DeletedAt
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, err 	// is this necessary????)
 		}
-	}
-	log.Println("Passou pelo rows.Next()")
-	if err = rows.Err(); err != nil {
-		log.Printf("err :%v\n", err)
 		return nil, err
 	}
+
+	log.Println("vai executar rows.Next()")
+	// for rows.Next() {
+	// 	log.Println("vai executar rows.Scan")
+	// 	columns, _ := rows.Columns()
+	// 	log.Printf("Columns: %v\n", columns)
+	// 	err = rows.Scan(&tr.Status, &tr.Value, &tr.ApprovedAt, &tr.DeniedAt, &tr.CreatedAt, &tr.UpdatedAt, &tr.DeletedAt)
+	// 	if err != nil {
+	// 		log.Printf("err :%v\n", err)
+	// 		return nil, err
+	// 	}
+	// }
+	// log.Println("Passou pelo rows.Next()")
+	// if err = rows.Err(); err != nil {
+	// 	log.Printf("err :%v\n", err)
+	// 	return nil, err
+	// }
 
 	transaction, err := entity.MountTransaction(
 		tr.ID,
